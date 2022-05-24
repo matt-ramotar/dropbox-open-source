82b10c0fd632086e751cc0b4ea20e626220e0d0d
mramotar <mramotar@dropbox.com>
2022-05-24 11:19:38 -0400

    Separate

--- fetch-action/fetch.js
+++ fetch-action/fetch.js
@@ -7,49 +7,8 @@ async function fetch(){
     try {
         const octokit = new Octokit()
 
-        const projects = []
-        const idToLanguages = {}
-
-        let page = 1
-        let shouldFetch = true
-
-        while (shouldFetch) {
-            const repositories = await fetchRepositories(octokit, page)
-
-            projects.push(...repositories)
-
-            if (repositories.length != 100) shouldFetch = false
-            else page++
-        }
- 
-        fs.writeFile('./projects.json', JSON.stringify(projects), err => {
-            console.log(err)
-        })
-
-        const projectsStr = `
-        const projects = [${projects.map(project => JSON.stringify(project))}];
-        export default projects;
-        `
-        
-        fs.writeFile('./projects.js', projectsStr, err => {
-            console.log(err)
-        })
-    
-        for (const project of projects) {
-            const languages = await fetchLanguages(project)
-            if (languages) {
-                idToLanguages[project.id] = languages
-            } 
-        }
-
-        const idToLanguagesStr = `
-        const idToLanguages = ${JSON.stringify(idToLanguages)};
-        export default idToLanguages;
-        `
-
-        fs.writeFile('./idToLanguages.js', idToLanguagesStr, err => {
-            console.log(err)
-        })
+        const projects = await handleProjects(octokit)
+        await handleLanguages(octokit, projects)
 
     } catch (e) {
         console.log(e)
@@ -68,6 +27,60 @@ async function fetchRepositories(octokit, page) {
 
 
     return data
+}
+
+async function handleProjects(octokit) {
+    const projects = []
+
+    let page = 1
+    let shouldFetch = true
+
+    while (shouldFetch) {
+        const repositories = await fetchRepositories(octokit, page)
+
+        projects.push(...repositories)
+
+        if (repositories.length != 100) shouldFetch = false
+        else page++
+    }
+
+    fs.writeFile('./projects.json', JSON.stringify(projects), err => {
+        console.log(err)
+    })
+
+    const projectsStr = `
+    const projects = [${projects.map(project => JSON.stringify(project))}];
+    export default projects;
+    `
+    
+    fs.writeFile('./projects.js', projectsStr, err => {
+        console.log(err)
+    })
+
+    return projects
+}
+
+async function handleLanguages(octokit, projects) {
+
+    const idToLanguages = {}
+
+       
+    for (const project of projects) {
+        const languages = await fetchLanguages(octokit, project)
+        if (languages) {
+            idToLanguages[project.id] = languages
+        } 
+    }
+
+    const idToLanguagesStr = `
+    const idToLanguages = ${JSON.stringify(idToLanguages)};
+    export default idToLanguages;
+    `
+
+    fs.writeFile('./idToLanguages.js', idToLanguagesStr, err => {
+        console.log(err)
+    })
+    
 }
 
 async function fetchLanguages(octokit, repository) {
