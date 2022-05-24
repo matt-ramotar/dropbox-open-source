5dade3deb090054c33253492a92a6b7e824d9290
mramotar <mramotar@dropbox.com>
2022-05-24 11:58:06 -0400

    Revert workflow

--- fetch-action/fetch.js
+++ fetch-action/fetch.js
@@ -1,79 +1,45 @@
-import { Octokit } from '@octokit/action';
+import axios from 'axios';
 import fs from 'fs';
 
-const REPOS_URL = "/orgs/dropbox/repos"
+const REPOS_URL = "https://api.github.com/orgs/dropbox/repos"
 
 async function fetch(){
     try {
-        const octokit = new Octokit()
+        const projects = []
 
-        const projects = await handleProjects(octokit)
-        await handleLanguages(octokit, projects)
+        let page = 1
+        let shouldFetch = true
 
+        while (shouldFetch) {
+            const response = await axios.get(REPOS_URL, {
+                params: {
+                    sort: "updated",
+                    per_page: 100,
+                    page: page
+                }
+            })
+
+            projects.push(...response.data)
+
+            if (response.data.length != 100) shouldFetch = false
+            else page++
+        }
+ 
+        fs.writeFile('./projects.json', JSON.stringify(projects), err => {
+            console.log(err)
+        })
+
+        fs.writeFile('./projects.js', `
+        const projects = [${projects.map(project => JSON.stringify(project))}];
+        export default projects;
+        `, err => {
+            console.log("Error writing projects JS", err)
+        })
+    
     } catch (e) {
         console.log(e)
         process.exit(-1)
     }
 };
-
-async function handleProjects(octokit) {
-    const projects = []
-
-    let page = 1
-    let shouldFetch = true
-
-    while (shouldFetch) {
-        console.log("Requesting page ", page)
-        const response = await octokit.request(`GET ${REPOS_URL}`, {
-            params: JSON.stringify({
-                sort: "updated",
-                per_page: 100,
-                page: page
-            })});
-
-        projects.push(...response.data)
-
-        if (response.data.length != 100) shouldFetch = false
-        else page++
-    }
-
-    fs.writeFile('./projects.json', JSON.stringify(projects), err => {
-        console.log("Error writing projects JSON", err)
-    })
-
-    const projectsStr = `
-    const projects = [${projects.map(project => JSON.stringify(project))}];
-    export default projects;
-    `
-    
-    fs.writeFile('./projects.js', projectsStr, err => {
-        console.log("Error writing projects JS", err)
-    })
-
-    return projects
-}
-
-async function handleLanguages(octokit, projects) {
-
-    const idToLanguages = {}
-
-       
-    for (const project of projects) {
-        const response = await octokit.request(`GET ${project.languages_url.replace("https://api.github.com", '')}`);
-        if (response.data) {
-            idToLanguages[project.id] = response.data
-        } 
-    }
-
-    const idToLanguagesStr = `
-    const idToLanguages = ${JSON.stringify(idToLanguages)};
-    export default idToLanguages;
-    `
-
-    fs.writeFile('./idToLanguages.js', idToLanguagesStr, err => {
-        console.log("Error writing idToLanguages", err)
-    })
-    
-}
 
 fetch()
